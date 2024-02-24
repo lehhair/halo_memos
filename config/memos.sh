7e4fd669876a9cdb10fcc9a2572bdd6abace1653
lehhair <82690391+lehhair@users.noreply.github.com>
2024-02-24 17:16:10 +0800

    Update memos.sh
    变更app可执行文件的位置

--- config/memos.sh
+++ config/memos.sh
@@ -5,7 +5,7 @@ export createdTs=$(echo $1 | jq -r '.memo.createdTs')
 
 export memos_date=$(date -u +%FT%TZ)
 if [ "$(echo $1 | jq -r '.memo.visibility')" == "PUBLIC" ] && [ "$(echo $1 | jq -r '.activityType')" == "memos.memo.created" ]; then
-  ./config/app
+  ./memos/app
 else
   # echo "bad" >> /config/logfile.txt
   echo "非公开发布请求"
