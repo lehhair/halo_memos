1b1f6852e10c6ee6b0ef4a44e2b7058608bdcdd9
lehhair <82690391+lehhair@users.noreply.github.com>
2024-03-05 12:08:11 +0800

    Update memos.sh
    状态交给py脚本处理

--- config/memos.sh
+++ config/memos.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
 export MEMOS_MESSAGE=$(echo $1 | jq -r '.memo.content')
 export createdTs=$(echo $1 | jq -r '.memo.createdTs')
-
-export memos_date=$(date -u +%FT%TZ)
-if [ "$(echo $1 | jq -r '.memo.visibility')" == "PUBLIC" ] && [ "$(echo $1 | jq -r '.activityType')" != "memos.memo.deleted" ]; then
+# export memos_date=$(date -u +%FT%TZ)
+export memos_date=$(date -u -d @"$createdTs" +%FT%TZ)
+export activity=$(echo $1 | jq -r '.activityType')
+if [ "$(echo $1 | jq -r '.memo.visibility')" == "PUBLIC" ]; then
   ./memos/app
 else
-  # echo "bad" >> /config/logfile.txt
-  echo "非公开发布请求"
+  # echo $1 >> /config/logfile.txt
+  echo "非公开请求"
 fi
